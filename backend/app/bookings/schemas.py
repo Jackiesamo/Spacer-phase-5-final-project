f6c1334bf1f195357b946295ae7faa88d78b3526
Elvin <elvin.mwarangu@student.moringaschool.com>
2026-02-05 13:57:22 +0300

    bookings schema code almost done

--- backend/app/bookings/schemas.py
+++ backend/app/bookings/schemas.py
@@ -2,3 +2,7 @@ from pydantic import BaseModel
 from uuid import UUID
 from datetime import datetime
 
+class BookingCreate(BaseModel):
+    space_id: UUID
+    start_time: datetime
+    end_time: datetime
